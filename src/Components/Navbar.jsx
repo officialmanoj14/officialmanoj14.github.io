51610556ccd55c33b9e4e8234875beec121ea1f3
Manoj Kumar <officailmanoj14@gmail.com>
2023-02-21 20:25:24 +0530

    update navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,27 +1,125 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom';
+import {
+  Box,
+  Flex,
+  HStack,
+  IconButton,
+  Menu,
+  MenuButton,
+  useDisclosure,
+  useColorModeValue,
+  Stack,
+} from "@chakra-ui/react";
+import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
+import { NavLink } from "react-router-dom";
 
-const Navbar = () => {
-    const links = [
-        {path:'/', title:'Home'},
-        {path:'/projects', title:'Projects'},
-        {path:'/skills', title:'Skills'},
-        {path:'/contacts', title:'Contacts'},
-        {path:'/about', title:'About'},
-    ]
+const Links = [
+  { path: "/", title: "Home" },
+  { path: "/about", title: "About" },
+  { path: "/skills", title: "Skills" },
+  { path: "/projects", title: "Projects" },
+  { path: "/contacts", title: "Contacts" },
+];
 
-    const navlinkStyle = ({ isActive }) => ({ 
-        color: isActive ? 'blue' : 'black' });
+export default function Navbar() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
-    return (
-        <div id='nav-menu'>
-            {
-                links.length && links.map((el) => {
-                    return <NavLink key={el.path} to={el.path} style={navlinkStyle}>{el.title}</NavLink>
-                })
+  return (
+    <>
+      <Box bg={useColorModeValue("#03045e")} px={4}>
+        <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
+          <IconButton
+            size={"md"}
+            bg={"#03045e"}
+            icon={
+              isOpen ? (
+                <CloseIcon color={"#00A3C4"} />
+              ) : (
+                <HamburgerIcon color={"#00A3C4"} />
+              )
             }
-        </div>
-    )
-}
+            aria-label={"Open Menu"}
+            display={{ md: "none" }}
+            onClick={isOpen ? onClose : onOpen}
+          />
+          <HStack spacing={8} alignItems={"center"} ml={5}>
+            <Box
+              fontWeight="bold"
+              fontSize={"27px"}
+              _hover={{ color: "#00A3C4" }}
+              color={"white"}
+              // border={'1px solid red'}
+              padding={"0px 2px 15px 2px"}
+            >
+              MK<span style={{ color: "#00A3C4", fontSize: "40px" }}>.</span>
+            </Box>
+          </HStack>
+          <Flex alignItems={"center"}>
+            <HStack mr={7} fontWeight={"bolder"} fontSize={"19px"}>
+              <HStack
+                as={"nav"}
+                spacing={4}
+                display={{ base: "none", md: "flex" }}
+                // color={"#00A3C4"}
+              >
+                {Links.map((link) => (
+                  <NavLink
+                    key={link.path}
+                    px={2}
+                    py={1}
+                    rounded={"md"}
+                    style={({ isActive }) => ({
+                      color: isActive ? 'white' : '#00A3C4'
+                    })}
+                    to={link.path}
+                  >
+                    {link.title}
+                  </NavLink>
+                ))}
+              </HStack>
+            </HStack>
+            <Menu>
+              <MenuButton
+                // as={Button}
+                color={"white"}
+                border={"1px solid #00A3C4"}
+                padding={"2px 35px 3px 35px"}
+                _hover={{ border: "1px solid white", color: "#00A3C4" }}
+                variant={"link"}
+                cursor={"pointer"}
+                borderRadius={"50px"}
+                fontSize={"20px"}
+                textDecoration={"none"}
+                mr={8}
+              >
+                Resume
+              </MenuButton>
+            </Menu>
+          </Flex>
+        </Flex>
 
-export default Navbar;
+        {isOpen ? (
+          <Box pb={4} display={{ md: "none" }}>
+            <Stack as={"nav"} spacing={4} fontSize={"19px"} fontWeight={"bold"}>
+              {Links.map((link) => (
+                <NavLink
+                  key={link.path}
+                  px={2}
+                  py={1}
+                  rounded={"md"}
+                  style={({ isActive }) => ({
+                    color: isActive ? 'white' : '#00A3C4'
+                  })}
+                  to={link.path}
+                >
+                  {link.title}
+                </NavLink>
+              ))}
+            </Stack>
+          </Box>
+        ) : null}
+      </Box>
+
+      <Box p={4}>Main Content Here</Box>
+    </>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { ChakraProvider } from '@chakra-ui/react';
+import customTheme from './Components/utils/Theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <ChakraProvider theme={customTheme}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ChakraProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
